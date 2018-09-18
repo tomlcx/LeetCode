5aaa72a8c12562dffe75c80cead4df91af77c411
tomlcx <krisliuyrpo@163.com>
2018-09-18 21:24:50 +0800

    Update MergeTwoSortedLists.java

--- algorithms/MergeTwoSortedLists/MergeTwoSortedLists.java
+++ algorithms/MergeTwoSortedLists/MergeTwoSortedLists.java
@@ -9,29 +9,30 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode l3 = new ListNode(0);
-        while(l1.next!=null && l2.next!=null){
-            if(l1.val > l2.val){
-                l3.next = l1;
+        ListNode l4 = l3;
+        while(l1!=null && l2!=null){
+            if(l1.val <= l2.val){
+                l4.next = l1;
                 l1 = l1.next;
-                l3 = l3.next;
+                l4 = l4.next;
             }
             else{
-                l3.next = l2;
+                l4.next = l2;
                 l2 = l2.next;
-                l3 = l3.next;
+                l4 = l4.next;
             }
         }
         
-        if(l1.next != null){
-            l3.next = l1;
+        if(l1 != null){
+            l4.next = l1;
             l1 = l1.next;
-            l3 = l3.next;
+            l4 = l4.next;
         }
-        if(l2.next != null){
-            l3.next = l2;
+        if(l2 != null){
+            l4.next = l2;
             l2 = l2.next;
-            l3 = l3.next;
+            l4 = l4.next;
         }
-        return l3;
+        return l3.next;
     }
 }
